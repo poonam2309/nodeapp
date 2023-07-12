ef46879a823aa1610a653e16b61d478fa9cc2ee9
poonam2309 <poonam.kadian09@gmail.com>
2023-07-12 17:07:38 +0000

    app updated

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
+
+// Accessing environment variables from the ConfigMap
+const databaseUrl = process.env.DATABASE_URL;
+const apiKey = process.env.API_KEY;
+
+// Accessing secret values from the Secrets
+const secretKey = process.env.SECRET_KEY;
+
+// Using the volume to store and read data
+const storagePath = '/app/storage/data.txt';
+
+app.get('/', (req, res) => {
+  // Perform operations using the environment variables, secrets, and volume
+  const data = `Database URL: ${databaseUrl}\nAPI Key: ${apiKey}\nSecret Key: ${secretKey}\n`;
+
+  fs.appendFile(storagePath, data, (err) => {
+    if (err) {
+      console.error('Error writing data:', err);
+      return res.status(500).send('Internal Server Error');
+    }
+
+    console.log('Data written successfully!');
+    res.send('Data written successfully!');
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
+
