95c43fcfc5e025c0990171944f2ad39f2bdaf973
poonam2309 <67383223+poonam2309@users.noreply.github.com>
2023-07-12 22:40:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,34 @@
-const http = require('http');
+const express = require('express');
+const fs = require('fs');
 
-const port = process.env.PORT || 3000;
+const app = express();
 
-http.createServer(function(request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end("Hello, World!\n");
-}).listen(port);
+// Accessing environment variables from the ConfigMap
+const databaseUrl = process.env.DATABASE_URL;
+const apiKey = process.env.API_KEY;
 
-console.log(`App is running... (port: ${port})`);
+// Accessing secret values from the Secrets
+const secretKey = process.env.SECRET_KEY;
+
+// Using the volume to store and read data
+const storagePath = '/app/storage/data.txt';
+
+app.get('/', (req, res) => {
+  // Perform operations using the environment variables, secrets, and volume
+  const data = `Database URL: ${databaseUrl}\nAPI Key: ${apiKey}\nSecret Key: ${secretKey}\n`;
+
+  fs.appendFile(storagePath, data, (err) => {
+    if (err) {
+      console.error('Error writing data:', err);
+      return res.status(500).send('Internal Server Error');
+    }
+
+    console.log('Data written successfully!');
+    res.send('Data written successfully!');
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
+
